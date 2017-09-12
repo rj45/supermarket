6c02269cb44b7ec6de8f3c51af431bf7b7f2236a
Ryan Sanche <ryan@rj45.ca>
2017-09-12 09:49:07 -0700

    Add more unit validation tests

--- test/register.js
+++ test/register.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const { Register, EACH } = require('../lib/register');
+const { Register, EACH, GRAMS, POUNDS, OUNCES } = require('../lib/register');
 
 describe('Register', () => {
   let register;
@@ -18,12 +18,19 @@ describe('Register', () => {
     };
     // TODO: may want more specific error messages than just 'field is invalid'
     const testCases = [
+      { field: 'sku', value: null, error: /sku is invalid/ },
       { field: 'sku', value: '', error: /sku is invalid/ },
+      { field: 'sku', value: 99, error: /sku is invalid/ },
+      { field: 'name', value: null, error: /name is invalid/ },
       { field: 'name', value: '', error: /name is invalid/ },
+      { field: 'name', value: 99, error: /name is invalid/ },
+      { field: 'unit', value: null, error: /unit is invalid/ },
       { field: 'unit', value: 'weird', error: /unit is invalid/ },
+      { field: 'qty', value: null, error: /qty is invalid/ },
       { field: 'qty', value: 0, error: /qty is invalid/ },
       { field: 'qty', value: -1, error: /qty is invalid/ },
       { field: 'qty', value: 'a string', error: /qty is invalid/ },
+      { field: 'price', value: null, error: /price is invalid/ },
       { field: 'price', value: 0, error: /price is invalid/ }, // no free products
       { field: 'price', value: -1, error: /price is invalid/ },
       { field: 'price', value: 'a string', error: /price is invalid/ },
@@ -33,11 +40,26 @@ describe('Register', () => {
       register.addProduct(validProduct);
     });
 
+    it('does not error with gram units', () => {
+      const product = modifyProductFieldWithValue('unit', GRAMS);
+      register.addProduct(product);
+    });
+
+    it('does not error with pounds unit', () => {
+      const product = modifyProductFieldWithValue('unit', POUNDS);
+      register.addProduct(product);
+    });
+
+    it('does not error with ounces unit', () => {
+      const product = modifyProductFieldWithValue('unit', OUNCES);
+      register.addProduct(product);
+    });
+
     // do table driven tests
     testCases.forEach(testCase => {
       const displayValue = handleEmptyString(testCase.value);
 
-      it(`fails when ${testCase.field} is ${displayValue}`, () => {
+      it(`errors when ${testCase.field} is ${displayValue}`, () => {
         const errantProduct = modifyProductFieldWithValue(
           testCase.field,
           testCase.value
