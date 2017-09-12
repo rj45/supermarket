8f5039b1e24808a63e6c96c0343ccac9b90f97bb
Ryan Sanche <ryan@rj45.ca>
2017-09-12 09:27:45 -0700

    Product is validated

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
   extends: 'airbnb-base',
+  globals: {
+    describe: true,
+    beforeEach: true,
+    it: true,
+  },
   rules: {
     // I want to use console statements for debugging
     'no-console': ['off'],
@@ -9,5 +14,20 @@ module.exports = {
 
     // this interferes with prettier and prettier can't be configured to always use parens
     'arrow-parens': ['error', 'as-needed'],
+
+    // I use this to denote private members
+    'no-underscore-dangle': ['off'],
+
+    // dangling commas on functions fail to parse on node 6 (works on node 8 though)
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'never',
+      },
+    ],
   },
 };
--- test/register.js
+++ test/register.js
@@ -9,50 +9,57 @@ describe('Register', () => {
   });
 
   describe('with a product validation', () => {
+    const validProduct = {
+      sku: 'cherios',
+      name: 'cherios',
+      unit: EACH,
+      qty: 1,
+      price: 4.99,
+    };
+    // TODO: may want more specific error messages than just 'field is invalid'
+    const testCases = [
+      { field: 'sku', value: '', error: /sku is invalid/ },
+      { field: 'name', value: '', error: /name is invalid/ },
+      { field: 'unit', value: 'weird', error: /unit is invalid/ },
+      { field: 'qty', value: 0, error: /qty is invalid/ },
+      { field: 'qty', value: -1, error: /qty is invalid/ },
+      { field: 'qty', value: 'a string', error: /qty is invalid/ },
+      { field: 'price', value: 0, error: /price is invalid/ }, // no free products
+      { field: 'price', value: -1, error: /price is invalid/ },
+      { field: 'price', value: 'a string', error: /price is invalid/ },
+    ];
+
     it('does not error with a valid product', () => {
-      register.addProduct({
-        sku: 'cherios',
-        name: 'cherios',
-        unit: EACH,
-        qty: 1,
-        price: 4.99,
+      register.addProduct(validProduct);
+    });
+
+    // do table driven tests
+    testCases.forEach(testCase => {
+      const displayValue = handleEmptyString(testCase.value);
+
+      it(`fails when ${testCase.field} is ${displayValue}`, () => {
+        const errantProduct = modifyProductFieldWithValue(
+          testCase.field,
+          testCase.value
+        );
+
+        assert.throws(() => {
+          register.addProduct(errantProduct);
+        }, testCase.error);
       });
     });
 
-    it('fails when the sku is falsy', () => {
-      assert.throws(() => {
-        register.addProduct({
-          sku: '',
-          name: 'cherios',
-          unit: EACH,
-          qty: 1,
-          price: 4.99,
-        });
-      }, /sku is invalid/);
-    });
+    function handleEmptyString(value) {
+      if (value === '') {
+        return 'empty string';
+      }
+      return value;
+    }
 
-    it('fails when the name is falsy', () => {
-      assert.throws(() => {
-        register.addProduct({
-          sku: 'cherios',
-          name: '',
-          unit: EACH,
-          qty: 1,
-          price: 4.99,
-        });
-      }, /name is invalid/);
-    });
-
-    it('fails when the unit is weird', () => {
-      assert.throws(() => {
-        register.addProduct({
-          sku: 'cherios',
-          name: 'cherios',
-          unit: 'weird',
-          qty: 1,
-          price: 4.99,
-        });
-      }, /unit is invalid/);
-    });
+    function modifyProductFieldWithValue(field, value) {
+      const errantProduct = Object.assign({}, validProduct); // make a clone
+      errantProduct[field] = value;
+      return errantProduct;
+    }
   });
 });
